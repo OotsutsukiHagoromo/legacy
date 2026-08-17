4c2198e4eeb7fd0a2deccd571acbcc5672911073
Rikudo <245939414+OotsutsukiHagoromo@users.noreply.github.com>
2026-08-17 05:22:10 +0300

    Мелкие правки; make_rikudo

---
+++ make_rikudo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Прошу не модифицировать именно этот скрипт и не удалять его. Он нужен, чтобы
+# собирать файл во временной папке, находящейся в оперативной памяти; там же
+# генерируются служебные файлы. Благодаря тому, что временная папка находится в
+# оперативной памяти, при перекомпиляции не происходит перезаписи файлов на
+# SSD. Бережём ресурсы диска
+# — Rikudo
+
+outdir="/tmp/legacy"
+
+do_pdflatex() {
+	pdflatex \
+		--shell-escape \
+		--output-directory "$outdir" \
+		main.tex
+}
+
+do_biblatex() {
+	biber \
+		--output-directory "$outdir" \
+		main
+}
+
+if [[ ! -a "$outdir" ]]
+then
+	mkdir -p "$outdir" 
+fi
+
+case $1 in
+	--all)
+		do_pdflatex
+		do_biblatex
+		do_pdflatex
+		;;
+
+	*)
+		do_pdflatex
+esac
